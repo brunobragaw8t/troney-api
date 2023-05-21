14dec32a0ac455b49c88c4e33ac42329f44a4b8e
Bruno Braga <brunobragaw8t@gmail.com>
2023-05-21 11:57:27 +0100

    fix: remove unnecessary request transformations

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-      transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
     }),
   );
+
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
