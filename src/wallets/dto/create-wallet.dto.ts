f2e26a2b5aa10a2e460f98b20faf743047b90896
Bruno Braga <brunobragaw8t@gmail.com>
2023-06-02 22:11:54 +0100

    feature: add user prop to CreateWalletDto along with other validations

--- src/wallets/dto/create-wallet.dto.ts
+++ src/wallets/dto/create-wallet.dto.ts
@@ -1,9 +1,15 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsMongoId, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateWalletDto {
   @IsString()
+  @IsNotEmpty()
   readonly name: string;
 
   @IsNumber()
+  @IsNotEmpty()
   readonly startingBalance: number;
+
+  @IsMongoId()
+  @IsNotEmpty()
+  readonly user: string;
 }
