80ef34c24e8f93f62dcd17b8c53b389d16c6f7fe
Bruno Braga <brunobragaw8t@gmail.com>
2023-06-28 22:26:11 +0100

    feature: create findByEmail method in UsersRepository

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserCommand } from './commands/create-user/create-user.command';
+import { GetUserByEmailQuery } from './commands/get-user-by-email/get-user-by-email.query';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -12,5 +13,9 @@ export class UsersRepository {
 
   async create(command: CreateUserCommand): Promise<User> {
     return await this.userModel.create(command);
+  }
+
+  async findByEmail(query: GetUserByEmailQuery): Promise<User> {
+    return await this.userModel.findOne({ email: query.email }).exec();
   }
 }
