ef405178708eea4295a5c6c2aedb6adf86f77ed7
Bruno Braga <brunobragaw8t@gmail.com>
2023-06-29 22:23:07 +0100

    refactor: remove now unused UsersService

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,18 +4,11 @@ import { CreateUserHandler } from './commands/create-user/create-user.handler';
 import { GetUserByEmailHandler } from './commands/get-user-by-email/get-user-by-email.handler';
 import { User, UserSchema } from './entities/user.entity';
 import { UsersRepository } from './users.repository';
-import { UsersService } from './users.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  providers: [
-    CreateUserHandler,
-    GetUserByEmailHandler,
-    UsersRepository,
-    UsersService,
-  ],
-  exports: [UsersService],
+  providers: [CreateUserHandler, GetUserByEmailHandler, UsersRepository],
 })
 export class UsersModule {}
