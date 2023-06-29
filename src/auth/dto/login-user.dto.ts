13eb3fa7f3b2443a4c7f38bf1adefc35b95dee85
Bruno Braga <brunobragaw8t@gmail.com>
2023-06-29 21:58:05 +0100

    chore: document LoginUserDto properties for Swagger

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginUserDto {
+  @ApiProperty({ description: 'User email' })
   @IsEmail()
   readonly email: string;
 
+  @ApiProperty({ description: 'User password' })
   @IsString()
   @IsNotEmpty()
   readonly password: string;
