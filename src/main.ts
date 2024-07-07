297f63b3af6ca2fa9b91043eb261c27cd8d8857f
Bruno Braga <brunobragaw8t@gmail.com>
2024-07-07 18:11:17 +0100

    chore: install CQRS and Swagger modules

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService<unknown, true>);
+
+  SwaggerModule.setup(
+    'docs',
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Troney')
+        .setDescription('Troney API description')
+        .setVersion('0.0.1')
+        .build(),
+    ),
+  );
 
   await app.listen(configService.get<number>('PORT'));
 }
