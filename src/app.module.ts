15c293e93e676e556ddc5780ff214f8ad71a5e6b
Bruno Braga <brunobragaw8t@gmail.com>
2024-07-07 16:23:41 +0100

    chore: install and config database connection
    - Install and config TypeORM
    - Install Joi and setup a type-safe env configuration
    - Update .env.example to add DB-related variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        PORT: Joi.number().port().default(3000),
+        DB_HOST: Joi.string().hostname(),
+        DB_PORT: Joi.number().port().default(3306),
+        DB_NAME: Joi.string(),
+        DB_USERNAME: Joi.string().default('root'),
+        DB_PASSWORD: Joi.string().empty(''),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        database: configService.get<string>('DB_NAME'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
