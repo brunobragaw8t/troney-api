cd42749f8f0ed9977729b599977327987523fe25
Bruno Braga <brunobragaw8t@gmail.com>
2024-07-07 18:30:51 +0100

    feature: create UsersService.findAll and its test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './aggregate/user.entity';
+import { Repository } from 'typeorm';
+
+const usersMock: User[] = [
+  {
+    id: '123',
+    email: 'user1@email.com',
+    password: 'abc',
+    name: 'User One',
+  },
+  {
+    id: '456',
+    email: 'user2@email.com',
+    password: 'def',
+    name: 'User Two',
+  },
+];
 
 describe('UsersService', () => {
   let service: UsersService;
+  let repo: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      imports: [],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            find: jest.fn().mockResolvedValue(usersMock),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repo = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll()', () => {
+    it('should return array of users', async () => {
+      const users = await service.findAll();
+      expect(users).toEqual(usersMock);
+    });
   });
 });
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './aggregate/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly repo: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return await this.repo.find();
+  }
+}
