b3672de035885b74ecc1a1e404075cad81afb936
Bruno Braga <brunobragaw8t@gmail.com>
2024-07-26 22:03:19 +0100

    chore: add Swagger decorators to UserResponseDto

--- src/users/dto/common/user-response.dto.ts
+++ src/users/dto/common/user-response.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UserResponseDto {
+  @ApiProperty({ description: 'ID' })
   readonly id: string;
+
+  @ApiProperty({ description: 'Email' })
   readonly email: string;
+
+  @ApiProperty({ description: 'Name' })
   readonly name: string;
 }
