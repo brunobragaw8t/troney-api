1142a8c0f62d3306d8d07ad5ae47de4afd972fab
Bruno Braga <brunobragaw8t@gmail.com>
2024-07-26 22:00:46 +0100

    fix: provide GetUsersHandler in UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './aggregate/user.entity';
 import { CqrsModule } from '@nestjs/cqrs';
 import { CreateUserHandler } from './commands/create-user/create-user.handler';
+import { GetUsersHandler } from './queries/get-users/get-users.handler';
 
 @Module({
   imports: [CqrsModule, TypeOrmModule.forFeature([User])],
-  providers: [UsersService, CreateUserHandler],
+  providers: [UsersService, CreateUserHandler, GetUsersHandler],
 })
 export class UsersModule {}
