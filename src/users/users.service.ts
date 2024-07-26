c4868a301b4041d2c5e5d08253af079e0a7b9ea6
Bruno Braga <brunobragaw8t@gmail.com>
2024-07-26 22:00:13 +0100

    feature: create UsersService.mapToResponseDto

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,12 +3,28 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './aggregate/user.entity';
 import { Repository } from 'typeorm';
 import { FindAllUsersFilter } from './types/find-all-users-filter.type';
+import { UserResponseDto } from './dto/common/user-response.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly repo: Repository<User>,
   ) {}
+
+  /**
+   * Map User entity to response DTO
+   *
+   * @param user User entity
+   *
+   * @returns User's response DTO
+   */
+  mapToResponseDto(user: User): UserResponseDto {
+    return {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+    };
+  }
 
   async findAll(filter?: FindAllUsersFilter): Promise<User[]> {
     return await this.repo.find({ where: filter });
