189e5afc2ea8deaaa6c472de0817e71bbab30cc4
Bruno Braga <brunobragaw8t@gmail.com>
2024-08-08 21:56:25 +0100

    feature: create methods to create and find user in UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { User } from './aggregate/user.entity';
 import { Repository } from 'typeorm';
 import { FindAllUsersFilter } from './types/find-all-users-filter.type';
 import { UserResponseDto } from './dto/common/user-response.dto';
+import { CreateUserDto } from './dto/create-user/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -24,6 +25,34 @@ export class UsersService {
       email: user.email,
       name: user.name,
     };
+  }
+
+  /**
+   * Create user
+   *
+   * @param dto DTO for user creation
+   *
+   * @returns User on success, null on failure
+   */
+  async create(dto: CreateUserDto): Promise<User | null> {
+    const res = await this.repo.insert(dto);
+
+    if (res.identifiers.length === 0) {
+      return null;
+    }
+
+    return await this.find(res.identifiers[0].id);
+  }
+
+  /**
+   * Find user
+   *
+   * @param id User's ID
+   *
+   * @returns User
+   */
+  async find(id: string): Promise<User | null> {
+    return await this.repo.findOneBy({ id });
   }
 
   /**
