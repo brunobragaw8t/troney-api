89fc0dfbfd5cf46542f7817683f25198fd27c483
Bruno Braga <brunobragaw8t@gmail.com>
2024-08-17 12:04:10 +0100

    feature: create update method in UsersRepository

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -4,6 +4,7 @@ import { User } from './aggregate/user.entity';
 import { Repository } from 'typeorm';
 import { FindAllUsersFilter } from './types/find-all-users-filter.type';
 import { CreateUserDto } from './dto/create-user/create-user.dto';
+import { UpdateUserDto } from './dto/update-user/update-user.dto';
 
 @Injectable()
 export class UsersRepository {
@@ -48,5 +49,25 @@ export class UsersRepository {
    */
   async findAll(filter?: FindAllUsersFilter): Promise<User[]> {
     return await this.repo.find({ where: filter });
+  }
+
+  /**
+   * Update user
+   *
+   * @param id User's ID
+   * @param dto DTO for user update
+   *
+   * @returns User on success, null on failure
+   */
+  async update(id: string, dto: UpdateUserDto): Promise<User | null> {
+    const user = await this.find(id);
+
+    if (user === null) {
+      return null;
+    }
+
+    const res = await this.repo.save({ id, ...dto });
+
+    return await this.find(res.id);
   }
 }
